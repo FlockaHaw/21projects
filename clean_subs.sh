ba9d9e089d82023eeaeb2139af58f1ec32dd18c5
BALOV <BalovDanVal@mail.ru>
2026-06-24 17:32:04 +0300

    garbage

--- clean_subs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Находит все подмодули (режим 160000) в индексе и удаляет их
-
-git ls-files --stage | grep ^160000 | while read -r mode hash stage path; do
-    echo "Удаление из индекса: $path"
-    git rm --cached "$path"
-done
-
-echo "Готово. Теперь выполните 'git add .' и проверьте статус."
